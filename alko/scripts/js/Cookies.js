990307b81449077ed3f398438bf9f66dbb75f276
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2023-11-20 15:55:32 +0200

    Added direct upload for the csv file from alkos own servers that the data is always uptodate

---
+++ alko/scripts/js/Cookies.js
@@ -0,0 +1,76 @@
+function setCookies(reset = false) {
+    let url = "./index.php";
+    let isFilterSet = false;
+
+    if (reset) {
+        document.cookie = "country= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+        document.cookie = "priceLow= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+        document.cookie = "priceHigh= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+        document.cookie = "type= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+        document.cookie = "energyLow= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+        document.cookie = "energyHigh= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+        document.cookie = "bottleSize= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+        window.location = url;
+        return;
+    } else {
+        let params = new URLSearchParams(window.location.search);
+        let index = document.getElementById("country").selectedIndex;
+        let selected = document.getElementById("country");
+        let priceLow = document.getElementById("priceLow").value;
+        let priceHigh = document.getElementById("priceHigh").value;
+        let type = document.getElementById("type").value;
+        let energyLow = document.getElementById("energyLow").value;
+        let energyHigh = document.getElementById("energyHigh").value;
+        let bottleSize = document.getElementById("bottleSize").value;
+
+        // Reset all filters before applying new ones
+        params.delete("country");
+        params.delete("priceLow");
+        params.delete("priceHigh");
+        params.delete("type");
+        params.delete("energyLow");
+        params.delete("energyHigh");
+        params.delete("bottleSize");
+
+        if (index !== 0) {
+            params.set("country", selected.value);
+            isFilterSet = true;
+        }
+        if (priceLow !== "") {
+            params.set("priceLow", priceLow);
+            isFilterSet = true;
+        }
+        if (priceHigh !== "") {
+            params.set("priceHigh", priceHigh);
+            isFilterSet = true;
+        }
+        if (type !== "") {
+            params.set("type", type);
+            isFilterSet = true;
+        }
+        if (energyLow !== "") {
+            params.set("energyLow", energyLow);
+            isFilterSet = true;
+        }
+        if (energyHigh !== "") {
+            params.set("energyHigh", energyHigh);
+            isFilterSet = true;
+        }
+        if (bottleSize !== "") {
+            params.set("bottleSize", bottleSize);
+            isFilterSet = true;
+        }
+
+        // Reset the page number to 0 when a new filter is applied
+        if (isFilterSet) {
+            
+            params.set("page", 0);
+        }
+
+        url += "?" + params.toString();
+    }
+
+    if (isFilterSet) {
+        window.location = url;
+    }
+}
---
+++ alko/scripts/php/List.php
@@ -0,0 +1,24 @@
+<?php
+// Array of countries
+$countries = [
+    'Argentiina', 'Armenia', 'Australia', 'Belgia', 'Bulgaria', 'Chile', 'Englanti', 'Espanja', 'Etelä-Afrikka',
+    'Georgia', 'Israel', 'Italia', 'Itävalta', 'Kosovo', 'Kreikka', 'Kroatia', 'Libanon', 'Moldova',
+    'Muu alkuperämaa', 'Peru', 'Pohjois-Makedonia', 'Portugali', 'Ranska', 'Ruotsi', 'Saksa', 'Serbia',
+    'Slovakia', 'Slovenia', 'Tsekki', 'Turkki', 'Ukraina', 'Unkari', 'Uruguay', 'Uusi-Seelanti', 'Yhdysvallat',
+    'Euroopan unioni', 'Sveitsi', 'Intia', 'Japani', 'Kanada', 'Kypros', 'Luxemburg', 'Romania', 'Suomi',
+    'Alkuperämaa vaihtelee', 'Barbados', 'Bermuda', 'Dominikaaninen tasavalta', 'Grenada', 'Guatemala', 'Guyana',
+    'Hollanti', 'Jamaika', 'Kolumbia', 'Kuuba', 'Latvia', 'Liettua', 'Mauritius', 'Nicaragua', 'Panama',
+    'Puerto Rico', 'Skotlanti', 'Tanska', 'Trinidad ja Tobago', 'Venezuela', 'Viro', 'Irlanti', 'Kiina',
+    'Meksiko', 'Wales', 'Islanti', 'Norja', 'Puola', 'Thaimaa', 'Etelä-Korea', 'Brasilia'
+];
+
+// Array of bottle sizes
+$bottleSizes = ['0,2 l', '0,33 l', '0,5 l', '0,7 l', '0,75 l', '1 l', '1,5 l', '2 l', '3 l'];
+
+// Array of product types
+$types = [
+    'punaviinit', 'roseeviinit', 'valkoviinit', 'rommit', 'konjakit', 'viskit', 'oluet', 'siiderit', 'juomasekoitukset',
+    'alkoholittomat', 'lahja- ja juomatarvikkeet', 'Jälkiruokaviinit, väkevöidyt ja muut viinit', 'Brandyt, Armanjakit ja Calvadosit',
+    'Ginit ja maustetut viinat', 'Liköörit ja Katkerot', 'kuohuviinit & samppanjat', 'vodkat ja viinat'
+];
+?>
